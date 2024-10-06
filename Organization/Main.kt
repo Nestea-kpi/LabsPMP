69568fc43f36e1fb45fb544baef7848930adb9c2
Nestea-kpi <102859250+Nestea-kpi@users.noreply.github.com>
2024-10-06 19:58:25 +0300

    Update Main.kt

--- Organization/Main.kt
+++ Organization/Main.kt
@@ -1,11 +1,11 @@
 package Organization
 
 fun main(){
-    val dev1 = Developer("Alice", "Smith", 3000.0, 3)
-    val dev2 = Developer("Bob", "Johnson", 3000.0, 6)
-    val designer = Designer("Charlie", "Brown", 2500.0, 4, 0.9)
+    val dev1 = Developer("Alice", "Smith", 2600.0, 4)
+    val dev2 = Developer("Bob", "Johnson", 2400.0, 2)
+    val designer = Designer("Charlie", "Brown", 3500.0, 8, 0.8)
 
-    val manager = Manager("Eve", "Davis", 4000.0, 7)
+    val manager = Manager("Eve", "Davis", 5700.0, 8)
     manager.addTeamMember(dev1)
     manager.addTeamMember(dev2)
     manager.addTeamMember(designer)
